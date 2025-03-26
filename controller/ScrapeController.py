a9bee37d499e1fa3408ebe669ff38ac9b3c570bd
Andreas Lundmark <andreas.erik.lundmark@gmail.com>
2025-03-26 14:13:39 +0100

    add endpoint call description

--- controller/ScrapeController.py
+++ controller/ScrapeController.py
@@ -4,7 +4,8 @@ from service.ScrapeService import ScrapeService
 router = APIRouter()
 scrape_service = ScrapeService()
 
-
+#Example of call
+#http://127.0.0.1:8000/articles/?base_url=https%3A%2F%2Fedition.cnn.com&search_word=yemen
 @router.get("/articles/")
 def get_articles(base_url: str, search_word: str):
     return scrape_service.scrape_url(base_url=base_url, search_word=search_word)
