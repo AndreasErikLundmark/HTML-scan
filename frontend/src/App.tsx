ee52643cf34ff7f0d39a598663a1ccda7c26579e
Andreas Lundmark <andreas.erik.lundmark@gmail.com>
2025-03-27 15:26:06 +0100

    frontend tailwind ok

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import Navbar from "./assets/Nav/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Navbar />
       <p>This is the start</p>
+      <button className="btn btn-primary">Click me</button>
     </>
   );
 }
