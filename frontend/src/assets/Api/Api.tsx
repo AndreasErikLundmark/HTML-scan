1a3190364994a64bd0ac51db7edb44bdd34b93ff
Andreas Lundmark <andreas.erik.lundmark@gmail.com>
2025-04-03 11:06:17 +0200

    add api fetch

--- frontend/src/assets/Api/Api.tsx
+++ frontend/src/assets/Api/Api.tsx
@@ -1,4 +1,4 @@
-import { ahrefResponseObject } from "../types/types";
+import { ahrefResponseObject, textResponseObject } from "../types/types";
 
 const GET_URL = "http://34.59.91.18/";
 
@@ -7,6 +7,36 @@ export const fetchBackend = async (
   search_target: string,
   search_word: string
 ): Promise<ahrefResponseObject[]> => {
+  const encodedBaseUrl = encodeURIComponent(base_url);
+  const encodedSearchTarget = encodeURIComponent(search_target);
+  const encodedSearchWord = encodeURIComponent(search_word);
+
+  const urlWithParams = `${GET_URL}?base_url=${encodedBaseUrl}&search_target=${encodedSearchTarget}&search_word=${encodedSearchWord}`;
+
+  try {
+    const response = await fetch(urlWithParams, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch aHrefs. Status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching aHrefs:", error);
+    throw error;
+  }
+};
+
+export const fetchText = async (
+  base_url: string,
+  search_target: string,
+  search_word: string
+): Promise<textResponseObject[]> => {
   const encodedBaseUrl = encodeURIComponent(base_url);
   const encodedSearchTarget = encodeURIComponent(search_target);
   const encodedSearchWord = encodeURIComponent(search_word);
