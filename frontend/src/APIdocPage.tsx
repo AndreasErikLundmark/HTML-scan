1df2ee1a9e1ca74e9199e316daff122c401fad1c
Andreas <andreas@Mac.home>
2025-04-11 10:58:59 +0200

    doc text

--- frontend/src/APIdocPage.tsx
+++ frontend/src/APIdocPage.tsx
@@ -1,11 +1,12 @@
 import DocFoldOut from "./assets/DocFoldout/DocFoldOut";
+import IntroText from "./assets/DocFoldout/IntroText";
 import Navbar from "./assets/Nav/Navbar"
 
 export default function APIdocPage(){
     return (
         <div>
             <Navbar/>
-            
+            <IntroText/>
             <DocFoldOut info="GET_links" title="/links..."/>
             <DocFoldOut info="GET_headings" title="/headings..."/>
             <DocFoldOut info="GET_paragraphs" title="/paragraphs..."/>
---
+++ frontend/src/assets/DocFoldout/IntroText.tsx
@@ -0,0 +1,16 @@
+export default function IntroText(){
+    
+    return (
+        <div className=" text-xs text-blue-950 p-4">
+            <ul>
+                <li><strong>Endpoint documentation</strong>Use the <code>GET /links</code> endpoint to retrieve links from a specific webpage based on a search term. Provide the <code>base_url</code> of the webpage and the <code>search_word</code> to find relevant links containing the specified term.</li>
+                <br></br>
+                <li><strong>Tip:</strong> Leave the <code>search_word</code> empty when calling to get all results from the target. For example, you can first scan for links, and then make another call on those links to scan for paragraphs, helping you gather a broader set of results on the topic.</li>
+                <br></br>
+                <li><strong>Note:</strong> Scanning shouldn't be used without the approval of [appropriate party or authority].</li>
+
+            </ul>
+        </div>
+    )
+    
+}
